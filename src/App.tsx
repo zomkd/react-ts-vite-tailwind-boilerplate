e9a6500d843c30f5d1c0a479c8f43b83133ab4f7
dull69 <dull69@mail.ru>
2022-08-18 22:08:13 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+function App() {
+  return (
+      <h1 className="font-bold text-3xl text-blue-900">
+       React + TS + Vite + TailwindCSS
+      </h1>
+  );
+}
+
+export default App
